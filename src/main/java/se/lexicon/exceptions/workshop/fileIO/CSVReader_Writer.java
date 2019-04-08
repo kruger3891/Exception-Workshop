a763c7a51c129549a0b90a63bffe13105b418c06
kruger3891 <kruger3891@gmail.com>
2019-04-08 13:39:30 +0200

    asd

---
+++ src/main/java/se/lexicon/exceptions/workshop/fileIO/CSVReader_Writer.java
@@ -0,0 +1,154 @@
+package se.lexicon.exceptions.workshop.fileIO;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CSVReader_Writer {
+	 /**
+     * This method getMaleFirstNames should use a try-catch-finally without resources
+     * Should catch FileNotFoundException and IOException
+     * You should also close the Buffered reader in the finally block
+     * @return List<String> of male firstnames
+     */
+	public static List<String> getMaleFirstNames(){
+
+		BufferedReader reader = null;
+		List<String> names = null;
+
+		try {
+			reader = Files.newBufferedReader(Paths.get("firstname_males.txt"));
+			names = reader.lines().flatMap(line -> Stream.of(line.split(","))).collect(Collectors.toList());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		} finally {
+			System.out.println(names);
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return names;
+	}
+
+
+
+    /**
+     * This method getFemaleFirstNames should make use of a try-catch with resources
+     * @return
+     */
+	public static List<String> getFemaleFirstNames() {
+
+		List<String> names = null;
+
+		BufferedReader reader;
+		try {
+			reader = Files.newBufferedReader(Paths.get("firstname_female.txt"));
+			names = reader.lines().flatMap(line -> Stream.of(line.split(","))).collect(Collectors.toList());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(names);
+
+		return names;
+	}
+	
+
+
+    /**
+     * This method fetches strings from a file and put them into a list
+     * This method might throw IOException which due to the throws clause need to
+     * be handled by the caller.
+     * @return List <String> of last names
+     * @throws IOException
+     */
+    public static List<String> getLastNames() throws IOException{
+
+        List<String> names = null;
+        BufferedReader reader = null;
+
+        try{
+                reader = Files.newBufferedReader(Paths.get("lastnames.txt"));
+                names = reader.lines()
+                .flatMap(line -> Stream.of(line.split(",")))
+                .collect(Collectors.toList());
+
+
+        }catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+            
+			if(reader!=null) reader.close();
+            
+        }
+        return names;
+    }
+
+    
+
+    public static void saveLastNames(List <String> lastNames){
+
+            BufferedWriter writer;
+			try {
+				writer = Files.newBufferedWriter(Paths.get("lastnames.txt"));
+				for(String toWrite : lastNames){
+	                writer.append(toWrite+",");
+	            }
+	            writer.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            
+      }
+
+    public static void saveFemaleNames(List <String> femaleNames){
+        BufferedWriter writer;
+		try {
+			writer = Files.newBufferedWriter(Paths.get("firstname_female.txt"));
+			for(String toWrite : femaleNames){
+                writer.append(toWrite+",");
+            }
+            writer.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+            
+
+    }
+
+
+
+    public static void saveMaleNames(List <String> maleNames){
+        BufferedWriter writer;
+		try {
+			writer = Files.newBufferedWriter(Paths.get("firstname_males.txt"));
+			for(String toWrite : maleNames){
+                writer.append(toWrite+",");
+            }
+            writer.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+            
+
+
+    }
+
+
+}
